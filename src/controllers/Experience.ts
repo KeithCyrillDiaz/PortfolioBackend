1e5ce0ae6dd7bb8520c42341b4cd104d17beedff
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-15 20:00:53 +0800

    added fetch experience and storeexperience endpoint

---
+++ src/controllers/Experience.ts
@@ -0,0 +1,47 @@
+import { ExperienceTypes } from 'constants/types'
+import { MessageLog } from '../constants/consoleLogsFunction'
+import { ErrorMessages } from '../constants/Messages'
+import express from 'express'
+import { createExperience } from '../models/Experiences'
+
+
+export const fetchExperiences = async(req: express.Request, res: express.Response) => {
+    try {
+        
+        MessageLog.Event("Fetching Experiences")
+
+    } catch (error) {
+        console.log("Internal Server Error", error)
+        res.status(500).json(ErrorMessages["Server Error"])
+        return 
+    }
+}
+
+
+export const storeExperiences = async (req:express.Request, res:express.Response) => {
+    try {
+        
+        MessageLog.Event("Storing Experiences")
+        
+        const data: ExperienceTypes = {...req.body}
+        const result = await createExperience(data)
+        if(!result) {
+            MessageLog.Error("Failed to store data")
+            res.status(500).json(ErrorMessages["Server Error"])
+            return 
+        }
+
+        MessageLog.Success("Experience data have been stored")
+        res.status(200).json({messages: {
+                code: 0,
+                message: "Experience data have been stored"
+            },
+            result
+        })
+
+    } catch (error) {
+        console.log("Internal Server Error", error)
+        res.status(500).json(ErrorMessages["Server Error"])
+        return 
+    }
+}

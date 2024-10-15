d67374b07d0058256b4bd8ab56c617634c3ea9ca
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-16 04:11:12 +0800

    modified files and added Authetication on index.ts

--- src/router/About/Experience.ts
+++ src/router/About/Experience.ts
@@ -1,12 +1,8 @@
 import express from 'express'
-import { getBackendAuthentication } from '../../middlewares/Authentication'
 import { fetchExperiences, storeExperiences } from '../../controllers/About/Experience'
 import { experienceInputValidation } from '../../middlewares/About/Experience';
 
 export default(router: express.Router) => {
-    // Apply the middleware to all routes under the /PortfolioBackend path
-    router.use("/PortfolioBackend", getBackendAuthentication);
-
     router.get("/PortfolioBackend/fetchExperiences",   fetchExperiences)
     router.post("/PortfolioBackend/storeExperiences", experienceInputValidation, storeExperiences)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import Experience from './About/Experience'
 import TechnicalSkills from './About/TechnicalSkills'
+import Authentication from './Authentication'
 
 const router = express.Router()
 
 export default(): express.Router => {
+    Authentication(router) // Apply authentication globally to /PortfolioBackend
     Experience(router)
     TechnicalSkills(router)
     return router

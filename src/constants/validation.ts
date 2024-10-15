8a4617550bc25000adb1f93143f3125abc12b846
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-16 03:31:33 +0800

    added validMonth for validation

---
+++ src/constants/validation.ts
@@ -0,0 +1,4 @@
+import { dateTypes } from "./types";
+
+
+export const ValidMonth: dateTypes[] = ['January', 'February', 'March','April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']

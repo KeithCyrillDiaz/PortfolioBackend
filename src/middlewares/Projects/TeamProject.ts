8bc019181807b0a2729817c015d84e0aeaaf6622
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-17 01:37:15 +0800

    added middleware validation for inputs

---
+++ src/middlewares/Projects/TeamProject.ts
@@ -0,0 +1,55 @@
+import { MessageLog } from 'constants/consoleLogsFunction'
+import { catchErrorLog, ErrorMessages } from 'constants/Messages'
+import { ValidMonth } from 'constants/validation'
+import express from 'express'
+
+
+export const inputValidationTeamProject = async (req: express.Request, res: express.Response, next:express.NextFunction) => {
+    try {  
+        MessageLog.Event("Validating Input")
+        const { startingMonth,
+            endingMonth,
+            MobileAndDesktop,
+            year,
+            projectType,
+            appName,
+            projectDetails,
+            introduction,
+            images,
+            video,
+            Technologies,
+            Members,
+
+        } = req.body
+
+        if(
+            !startingMonth ||
+            !ValidMonth.includes(startingMonth) ||
+            !endingMonth ||
+            !ValidMonth.includes(endingMonth) ||
+            !MobileAndDesktop ||
+            !year ||
+            typeof year !== 'number' ||
+            !projectType ||
+            !appName ||
+            !projectDetails ||
+            !introduction ||
+            !Array.isArray(images) ||
+            !video ||
+            !Array.isArray(Technologies) ||
+            !Array.isArray(Members)
+
+        ) {
+            MessageLog.Error("Bad Request")
+            res.status(400).json(ErrorMessages['Bad Request'])
+            return
+        }
+            MessageLog.Ready("Inputs are Valid");
+
+            next();
+
+    } catch (error) {
+        catchErrorLog(res, error)
+        return
+    }
+}
